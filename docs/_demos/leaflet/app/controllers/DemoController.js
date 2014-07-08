b9a9151bc18d01396c15db980ab6ae21a6f79413
Robin Komiwes <robin.komiwes@gmail.com>
2014-07-08 14:16:01 +0200

    removed log

--- docs/_demos/leaflet/app/controllers/DemoController.js
+++ docs/_demos/leaflet/app/controllers/DemoController.js
@@ -1,7 +1,5 @@
 angular.module('demo')
     .controller('DemoController', ['$scope', 'PanelService', function($scope, panelService) {
-        console.log("zob");
-
         panelService.open('right', {
             templateUrl: 'demopanel.phtml',
             push: true,
