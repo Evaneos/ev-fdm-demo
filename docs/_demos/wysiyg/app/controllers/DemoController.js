5fd82af002d218a4dbebf21e503711e7747c87f2
Michael FORASTE <michael.foraste@gmail.com>
2014-07-24 12:17:21 +0200

    adding wysiwyg directive

---
+++ docs/_demos/wysiyg/app/controllers/DemoController.js
@@ -0,0 +1,8 @@
+angular.module('demo')
+    .controller('DemoController', ['$scope', 'PanelService', function($scope, panelService) {
+        panelService.open('right', {
+            templateUrl: 'demopanel.phtml',
+            push: true,
+            controller: 'PanelController'
+        });
+}]);
