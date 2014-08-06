66d02432060a173ddc63423533519a5f6790b7dd
johan <johan@evaneos.com>
2014-08-06 10:51:16 +0200

    Updating wysiyg demo

--- docs/_demos/wysiyg/app/app.js
+++ docs/_demos/wysiyg/app/app.js
@@ -3,5 +3,5 @@
 
 var app = angular.module('wysiwyg-demo', [ 'ui.router', 'ev-fdm', 'ev-tinymce'])
     .controller('demo', function ($scope) {
-        $scope.demoText = "The ship is more dosi now than star. united and mechanically strange.Proud, gutless scabbards begrudgingly trade a rainy, cold landlubber.";
+        $scope.demoText = "Lorem. (etc). ";
     });
