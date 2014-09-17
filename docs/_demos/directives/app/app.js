64bdebe501e35da04d39afa33cd55aab99e079a6
cyrilf <cyrilf7x@gmail.com>
2014-09-17 18:30:14 +0200

    [Component] ev-tag-list

--- docs/_demos/directives/app/app.js
+++ docs/_demos/directives/app/app.js
@@ -1,6 +1,27 @@
-(function() {
-  'use strict';
-  angular.module('demo', ['ev-fdm']);
+'use strict';
+var app = angular.module('demo', ['ev-fdm']);
 
 
-}) ();
+app.controller('TagController', ['$scope', function($scope) {
+  $scope.context = {
+    editMode: false,
+    tags: [
+      { name: 'Tartiflette'},
+      { name: 'Endives au jambon'},
+      { name: 'Petit salé'},
+      { name: 'Ouiche'},
+      { name: 'Camembert'},
+      { name: 'Saucisses de morteaux'},
+      { name: 'Lasagnes'}
+    ]
+  };
+
+  $scope.newPlate = {
+    name: ''
+  };
+
+  $scope.addPlate = function() {
+    $scope.context.tags.unshift(angular.copy($scope.newPlate));
+    $scope.newPlate.name = '';
+  };
+}]);
