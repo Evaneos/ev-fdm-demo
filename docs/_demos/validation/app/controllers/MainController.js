637ecb8cda9879fbeca3a700892e2d5ec9e16556
Robin Komiwes <robin.komiwes@gmail.com>
2014-11-27 11:23:48 +0100

    MainController cleaning

--- docs/_demos/validation/app/controllers/MainController.js
+++ docs/_demos/validation/app/controllers/MainController.js
@@ -2,8 +2,6 @@
 angular.module('demo')
     .controller('MainController', [ '$scope', function($scope) {
         $scope.user = {};
-        $scope.password = "";
-
         $scope.submitted = 0;
         $scope.emailPattern = /[^\s@]+@[^\s@]+\.[^\s@]+/
 
